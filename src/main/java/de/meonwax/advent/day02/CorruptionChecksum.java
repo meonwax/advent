0f8cd1da726e7f43e363c32cf23daf6e256d11d8
meonwax <meonwax@meonwax.de>
2017-12-02 23:10:00 +0100

    Add day 02 solution

---
+++ src/main/java/de/meonwax/advent/day02/CorruptionChecksum.java
@@ -0,0 +1,57 @@
+package de.meonwax.advent.day02;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class CorruptionChecksum {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        System.out.println("Checksum part one: " + loadInput().mapToInt(CorruptionChecksum::getRowChecksumOne).sum());
+        System.out.println("Checksum part two: " + loadInput().mapToInt(CorruptionChecksum::getRowChecksumTwo).sum());
+    }
+
+    private static int getRowChecksumOne(String row) {
+        Pattern pattern = Pattern.compile("\\s");
+        Integer maxValue = (pattern
+                .splitAsStream(row)
+                .mapToInt(Integer::valueOf)
+                .max())
+                .getAsInt();
+        Integer minValue = pattern.splitAsStream(row)
+                .mapToInt(Integer::valueOf)
+                .min()
+                .getAsInt();
+        return maxValue - minValue;
+    }
+
+    private static int getRowChecksumTwo(String row) {
+        int[] numbersInRow = getNumbersInRow(row).toArray();
+        for (int number1 : numbersInRow) {
+            for (int number2 : numbersInRow) {
+                if ((number1 != number2) && number1 % number2 == 0) {
+                    return number1 / number2;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static IntStream getNumbersInRow(String row) {
+        Pattern pattern = Pattern.compile("\\s");
+        return pattern
+                .splitAsStream(row)
+                .mapToInt(Integer::valueOf);
+    }
+
+    private static Stream<String> loadInput() throws IOException, URISyntaxException {
+        String fileName = "day02/input.txt";
+        URI uri = ClassLoader.getSystemResource(fileName).toURI();
+        return Files.lines(Paths.get(uri));
+    }
+}
