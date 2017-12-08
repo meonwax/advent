14d96b5416265ff326a9a176775b2a50f75caba2
meonwax <meonwax@meonwax.de>
2017-12-08 13:49:45 +0100

    Day 08 part one

---
+++ src/main/java/de/meonwax/advent/day08/Registers.java
@@ -0,0 +1,78 @@
+package de.meonwax.advent.day08;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Registers {
+
+    private static final Pattern PATTERN = Pattern.compile("([a-z]+) ([incde]+) ([-0-9]+) if ([a-z]+) ([!=<>]{1,2}) ([-0-9]+)");
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        System.out.println("Largest value in any register is: " + getMaxValue(loadInput()));
+    }
+
+    private static int getMaxValue(String[] input) throws IOException, URISyntaxException {
+        Map<String, Integer> registers = new HashMap<>();
+        for (String row : input) {
+            Matcher m = PATTERN.matcher(row);
+            if (m.find()) {
+                if (isCondition(registers, registers.get(m.group(4)), m.group(5), Integer.valueOf(m.group(6)))) {
+                    String register = m.group(1);
+                    Integer content = doOperation(registers.get(register), m.group(2), Integer.valueOf(m.group(3)));
+                    registers.put(register, content);
+                }
+            }
+        }
+        return registers.values().stream().reduce(Integer::max).get();
+    }
+
+    private static int doOperation(Integer content, String operation, Integer value) {
+        if (content == null) {
+            content = 0;
+        }
+        switch (operation) {
+            case "inc":
+                return content + value;
+            case "dec":
+                return content - value;
+            default:
+                return content;
+        }
+    }
+
+    private static boolean isCondition(Map<String, Integer> registers, Integer content, String operator, int value) {
+        if (content == null) {
+            content = 0;
+        }
+        switch (operator) {
+            case ">":
+                return content > value;
+            case "<":
+                return content < value;
+            case "==":
+                return content == value;
+            case "!=":
+                return content != value;
+            case ">=":
+                return content >= value;
+            case "<=":
+                return content <= value;
+            default:
+                return false;
+        }
+    }
+
+    private static String[] loadInput() throws IOException, URISyntaxException {
+        String fileName = "day08/input.txt";
+        URI uri = ClassLoader.getSystemResource(fileName).toURI();
+        return Files.lines(Paths.get(uri)).toArray(String[]::new);
+    }
+}
+
