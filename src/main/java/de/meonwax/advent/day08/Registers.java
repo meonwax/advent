5a001d6d40b90309a494e5c24b2fd08cf35d6e37
meonwax <meonwax@meonwax.de>
2017-12-08 13:57:32 +0100

    Day 08 part two

--- src/main/java/de/meonwax/advent/day08/Registers.java
+++ src/main/java/de/meonwax/advent/day08/Registers.java
@@ -5,8 +5,11 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,21 +18,24 @@ public class Registers {
     private static final Pattern PATTERN = Pattern.compile("([a-z]+) ([incde]+) ([-0-9]+) if ([a-z]+) ([!=<>]{1,2}) ([-0-9]+)");
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        System.out.println("Largest value in any register is: " + getMaxValue(loadInput()));
-    }
-
-    private static int getMaxValue(String[] input) throws IOException, URISyntaxException {
+        Set<Integer> maxValues = new HashSet<>();
         Map<String, Integer> registers = new HashMap<>();
-        for (String row : input) {
+        for (String row : loadInput()) {
             Matcher m = PATTERN.matcher(row);
             if (m.find()) {
                 if (isCondition(registers, registers.get(m.group(4)), m.group(5), Integer.valueOf(m.group(6)))) {
                     String register = m.group(1);
                     Integer content = doOperation(registers.get(register), m.group(2), Integer.valueOf(m.group(3)));
                     registers.put(register, content);
+                    maxValues.add(getMaxValue(registers));
                 }
             }
         }
+        System.out.println("Largest value at the end is: " + getMaxValue(registers));
+        System.out.println("Largest value overall is: " + Collections.max(maxValues));
+    }
+
+    private static int getMaxValue(Map<String, Integer> registers) {
         return registers.values().stream().reduce(Integer::max).get();
     }
 
@@ -69,7 +75,7 @@ public class Registers {
         }
     }
 
-    private static String[] loadInput() throws IOException, URISyntaxException {
+    private static String[] loadInput() throws URISyntaxException, IOException {
         String fileName = "day08/input.txt";
         URI uri = ClassLoader.getSystemResource(fileName).toURI();
         return Files.lines(Paths.get(uri)).toArray(String[]::new);
